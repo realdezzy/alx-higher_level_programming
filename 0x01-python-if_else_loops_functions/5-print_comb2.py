486d46350c622365735e1c50476b0c2fda1b9fef
realdezzy <realdezzy1@gmail.com>
2022-12-09 23:45:12 -0800

    more corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    print("{:02}".format(x), end=", ") if (x != 99) else print("{:d}".format(x))
+    print("{:02}".format(x), end=", ") \
+        if (x != 99) else print("{:d}".format(x))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,10 @@
 
 def uppercase(str):
     """uppercase converts string to uppercase."""
-    for i in str:
+    str_n = str +'\n'
+    for i in str_n:
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
             print("{}".format(i), end="")
         else:
             print("{}".format(i),end="")
-    print()
