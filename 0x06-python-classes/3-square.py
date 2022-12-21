848d02b7d80ea033bf0b3bf2d906a82efc6167bd
realdezzy <realdezzy1@gmail.com>
2022-12-20 17:49:47 -0800

    Pycodestyle

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,11 +14,13 @@ class Square:
         ValueError: if vslue of size is less than 0
     """
     def __init__(self, size=0):
+
         if type(size).__name__ != 'int':
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Returns the current square area
 
