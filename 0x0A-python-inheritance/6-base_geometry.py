53578f31e1281e0befb5e3bc3450c5655493b15a
realdezzy <realdezzy1@gmail.com>
2023-01-09 08:38:42 -0800

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Base Geometry"""
+
+
+class BaseGeometry:
+    """Base class for Geometric shapes"""
+
+    def area(self):
+        """Raises an exception"""
+
+        raise NameError("area() is not implemented")
