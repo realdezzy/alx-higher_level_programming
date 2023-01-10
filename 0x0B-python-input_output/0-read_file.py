1e5e7c120c107d9a0800d965d3195acb1510d3e7
realdezzy <realdezzy1@gmail.com>
2023-01-10 09:12:38 -0800

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,8 +5,7 @@
 def read_file(filename=""):
     """Reads textfile and prints to stdout"""
 
-    if not filename:
-        return
-    with open(filename, "r", encoding="utf-8") as f:
+
+    with open(filename, encoding="utf-8") as f:
         text = f.read()
-    print(text)
+    print(text, end="")
