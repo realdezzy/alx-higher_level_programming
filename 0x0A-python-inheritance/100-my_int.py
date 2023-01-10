00f16e655a983b0b0b0c6320b46bbc133aa362cd
realdezzy <realdezzy1@gmail.com>
2023-01-09 16:39:11 -0800

    Advanced tasks

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+"""MyInt equality reverser"""
+
+
+class MyInt(int):
+    """Implements reverse of equality operation"""
+
+    def __eq__(self,value):
+        """compares equality"""
+
+        return not super().__eq__(value)
+
+    def __ne__(self,value):
+        """Not equal comparison"""
+
+        return not super().__ne__(value)
