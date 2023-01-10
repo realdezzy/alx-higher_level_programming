9d41ddb7f2af21e7c17be51b42dcd2df7bd7e599
realdezzy <realdezzy1@gmail.com>
2023-01-09 16:45:39 -0800

    Style

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,12 +5,12 @@
 class MyInt(int):
     """Implements reverse of equality operation"""
 
-    def __eq__(self,value):
+    def __eq__(self, value):
         """compares equality"""
 
         return not super().__eq__(value)
 
-    def __ne__(self,value):
+    def __ne__(self, value):
         """Not equal comparison"""
 
         return not super().__ne__(value)
