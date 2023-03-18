b886a8c2369a9a1a204659bb8856dd08bb2f30ae
realdezzy <realdezzy1@gmail.com>
2023-03-18 07:05:21 -0700

    For Checker

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Select Cities module
+SelectStates module
 """
 import MySQLdb
 import sys
 
 
 def find_cities():
-    """Finds cities with the given state name in database"""
+    """Selects all cities in database"""
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    search_term = sys.argv[4]
+    searched = sys.argv[4]
 
     db = MySQLdb.connect(host='localhost',
                          port=3306,
@@ -25,18 +25,16 @@ def find_cities():
                 FROM cities\
                 INNER JOIN states\
                 ON cities.state_id = states.id\
-                WHERE states.name = %(search_state)s\
+                WHERE states.name = %(state_name)s\
                 ORDER BY cities.id ASC",
-                {'search_state': search_term})
-
+                {'state_name': searched}
+                )
     rows = cur.fetchall()
-    inc = 1
+    lst = []
     for row in rows:
-        if inc < len(rows):
-            print(row[0], end=',')
-            inc += 1
-        else:
-            print(row[0])
+        for col in row:
+            lst.append(col)
+    print(', '.join(lst))
     cur.close()
     db.close()
 
